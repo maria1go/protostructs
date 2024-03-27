a0a26e5d4300594b7f0e180905275c0208431157
maria1go <165020879+maria1go@users.noreply.github.com>
2024-03-27 13:37:39 +0200

    Update step7.py

--- step7.py
+++ step7.py
@@ -3,8 +3,10 @@
 
 #Extracting identifiers, identifiers+sequences from Foldseek output, writing seq + str .fasta
 #1) Identifiers are used in FoldTree for obtaining .pdb structures
-#2) identifiers+sequences are written to .fasta file for MSA and visualization
+#2) identifiers+sequences (str) are written to .fasta file for MSA and visualization
 #3) 2 .fasta files are combined for MSA of proteins found both by sequence-based + structure-based approaches.
+#####!seq.fasta is not appended to str.fasta directly since it was filtered by HHfilter after MSA! 
+#####The ids are mapped between filtered.msa. and seq.fasta, and only seqs of proteins present in filtered.msa are appended to combined file.
 
 m8_file = "/home/vmadmin/pipeline/alis_afdb-swissprot.m8"
 output_file = "identifiers.txt"
@@ -16,16 +18,16 @@ identifiers = []
 sequences = []
 
 #Reading query id+sequence since it is absent in .m8 file
+
 with open("alignment.fasta", "r") as f:
     sequence_id = ""
     seq = ""
     for line in f:
         if line.startswith(">"):
             sequence_id = line.strip().split('.')[0][1:]
-            identifiers.append(sequence_id)
-            break
+            identifiers.append(sequence_id) 
         else:
-            seq = line.strip()
+            seq += line.strip()
             sequences.append(seq)
             break
 
@@ -71,9 +73,55 @@ with open(output_file2, 'w') as f:
 print("Identifiers written to the foldtree directory.")
 
 
-#FASTA combined
-with open("alignment.fasta", 'r') as fasta1, open(fasta_file, 'r') as fasta2:
-    fasta1_seq = fasta1.read()
+#FILTERING
+
+filtered_ids = set()
+with open("msa.filtered.fasta", 'r') as filtered_file:
+    for line in filtered_file:
+        if line.startswith(">"):
+            filtered_ids.add(line.strip().split('.')[0][1:])
+print(filtered_ids)
+
+
+
+identifiers_aln = []
+sequences_aln = []
+
+with open("alignment.fasta", "r") as aln:
+    id_aln = ""
+    seq_aln = ""
+    for line in aln:
+        if line.startswith(">"):
+            if id_aln and seq_aln:
+                identifiers_aln.append(id_aln)
+                sequences_aln.append(seq_aln)
+            id_aln = line.strip().split('.')[0][1:]
+            seq_aln = ""
+            identifiers_aln.append(id_aln)
+        else:
+            seq_aln += line.strip()
+
+    if id_aln and seq_aln:
+        sequences_aln.append(seq_aln)
+
+print(identifiers_aln)
+
+with open("filtered.alignment.fasta", 'w') as content_fasta:
+    for id_aln, seq_aln in zip (identifiers_aln, sequences_aln):
+#        print("id_aln:", id_aln)
+#        print(seq_aln)
+        if id_aln in filtered_ids:
+            content_fasta.write(">" + id_aln + "\n" + seq_aln + "\n")
+
+print("Written", content_fasta)
+
+##PREVIOUS VERSION - UNFILTERED SEQUENCES FROM ALIGNMENT.FASTA WERE ALL WRITTEN TO THE NEW FILE
+
+
+#FASTA COMBINED
+
+with open("filtered.alignment.fasta", 'r') as fasta, open(fasta_file, 'r') as fasta2:
+    fasta1_seq = fasta.read()
     fasta2_seq = fasta2.read()
 
 combined_seq = fasta1_seq + '\n' + fasta2_seq
@@ -82,3 +130,4 @@ with open(combined_file, 'w') as output:
     output.write(combined_seq)
 
 print('FASTA files combined')
+
