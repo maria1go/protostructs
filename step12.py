42bee469e47da66b1d104333ae37d9172429212b
Tallivm <30693065+Tallivm@users.noreply.github.com>
2024-03-31 19:40:38 +0300

    Update step12.py
    Removed absolute paths, moved print into main

--- step12.py
+++ step12.py
@@ -1,20 +1,17 @@
 #MSA for combined sequences
 
 import subprocess
-mafft = "/usr/bin/mafft"
 
 input_file = "combined.fasta"
 output_file = "combined.msa.fasta"
 
 def align_sequences(input_file, output_file):
-    mafft_command = f"/usr/bin/mafft --auto --reorder {input_file} > {output_file}"
+    mafft_command = f"mafft --auto --reorder {input_file} > {output_file}"
     subprocess.call(mafft_command, shell=True)
 
 def main():
     align_sequences("combined.fasta", "combined.msa.fasta")
+    print("MSA completed.", output_file)
 
 if __name__=="__main__":
     main()
-
-print("MSA completed.", output_file)
-
