808f0f4be76d4f76f0ec91e96b24f6937dd29a8c
maria1go <165020879+maria1go@users.noreply.github.com>
2024-04-15 17:09:16 +0300

    Update step4.py

--- step4.py
+++ step4.py
@@ -3,17 +3,16 @@ import os
 
 
 identifiers = []
-with open("msa.filtered.fasta", 'r') as f:
+with open("results/msa.filtered.fasta", 'r') as f:
     for line in f:
         if line.startswith('>'):
             identifier = line.strip().split('.')[0][1:]
             identifiers.append(identifier)
 
-##TRANSFER IDENTIFIERS TO RIGHT FOLDERS
+##TRANSFER IDENTIFIERS TO FOLDTREE
 
-#output_folders = ["/home/vmadmin/fold_tree/fromseq", "/home/vmadmin/pipeline"]
-output_folder = "/home/vmadmin/fold_tree/fromseq"
-
+output_folder_name = "fold_tree"
+output_folder = os.path.join(os.path.expanduser("~"), output_folder_name)  
 os.makedirs(output_folder, exist_ok=True)
 
 output_file = os.path.join(output_folder, "identifiers.txt")
@@ -23,5 +22,23 @@ with open(output_file, 'w') as f:
     for identifier in identifiers:
         f.write(identifier + '\n')
 
-
 print("Identifiers written to:", output_file)
+
+##COPY IDENTIFIERS.TXT TO RESULTS 
+import shutil
+
+ids_seq_dirname = "fold_tree/fromseq"
+ids_seq = os.path.join(os.path.expanduser("~"), ids_seq_dirname)
+source = os.path.join(ids_seq, "identifiers.txt")
+
+
+pipeline_ids_dirname = "protostructs/results/identifiers_seq.txt"
+pipeline_ids = os.path.join(os.path.expanduser("~"), pipeline_ids_dirname)
+destination = pipeline_ids
+
+try:
+    shutil.copy(source, destination)
+    print("identifiers copied to results folder")
+except OSError as e:
+    print("Error", e)
+
