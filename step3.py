e37fe0dd1f47317f2b936e9524a62fcca1a58093
maria1go <165020879+maria1go@users.noreply.github.com>
2024-04-15 17:07:14 +0300

    Update step3.py

--- step3.py
+++ step3.py
@@ -2,15 +2,15 @@ import subprocess
 
 #HHfilter.
 #output .fasta required
-input_file = "msa.fasta"
-output_file = "msa.filtered.fasta"
+input_file = "results/msa.fasta"
+output_file = "results/msa.filtered.fasta"
 
 def filter_sequences(input_file, output_file):
     hhfilter_command = f"/usr/bin/hhfilter -M first -i {input_file} -o {output_file}"
     subprocess.call(hhfilter_command, shell=True)
 
 def main():
-    filter_sequences("msa.fasta", "msa.filtered.fasta")
+    filter_sequences("results/msa.fasta", "results/msa.filtered.fasta")
 
 if __name__=="__main__":
     main()
