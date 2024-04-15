0c5529dfa1b054b5e26fcde831c6a3c2dbcc3314
maria1go <165020879+maria1go@users.noreply.github.com>
2024-04-15 17:21:52 +0300

    Update step13.py

--- step13.py
+++ step13.py
@@ -73,5 +73,5 @@ def plot_msa_coverage(filename: str, mapping: dict = None,
                   curve_color=curve_color, gap_int=mapping['-'], savename=savename)
 
 
-filename = 'combined.msa.fasta'
-plot_msa_coverage(filename, figsize=(30, 5),savename='combined_alignment_coverage.png', curve_alpha=0.5)
+filename = 'results/combined.msa.fasta'
+plot_msa_coverage(filename, figsize=(30, 5),savename='results/combined_alignment_coverage.png', curve_alpha=0.5)
