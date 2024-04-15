8bb5c038c9ea9e91e6c8e920744391d4c41612d1
maria1go <165020879+maria1go@users.noreply.github.com>
2024-04-15 17:06:20 +0300

    Update step2.py

--- step2.py
+++ step2.py
@@ -1,15 +1,15 @@
 import subprocess
 mafft = "/usr/bin/mafft"
 
-input_file = "alignment.fasta"
-output_file = "msa.fasta"
+input_file = "results/alignment.fasta"
+output_file = "results/msa.fasta"
 
 def align_sequences(input_file, output_file):
     mafft_command = f"/usr/bin/mafft --auto --reorder {input_file} > {output_file}"
     subprocess.call(mafft_command, shell=True)
 
 def main():
-    align_sequences("alignment.fasta", "msa.fasta")
+    align_sequences("results/alignment.fasta", "results/msa.fasta")
 
 if __name__=="__main__":
     main()
