48f4761416881ed69044d12b269ae07c217162cd
maria1go <165020879+maria1go@users.noreply.github.com>
2024-04-16 11:32:51 +0300

    Update step4.py
    Added output folder, os.path.expanduser is now used for paths, identifiers.txt is additionally written to results as identifiers_seq.txt

--- step4.py
+++ step4.py
@@ -1,18 +1,19 @@
 import os
+import shutil
 #Extracting protein identifiers from MSA file
 
 
 identifiers = []
-with open("msa.filtered.fasta", 'r') as f:
+with open("results/msa.filtered.fasta", 'r') as f:
     for line in f:
         if line.startswith('>'):
             identifier = line.strip().split('.')[0][1:]
             identifiers.append(identifier)
+            
+##TRANSFER IDENTIFIERS TO FOLDTREE
 
-##TRANSFER IDENTIFIERS TO RIGHT FOLDERS
-
-#output_folders = ["/home/vmadmin/fold_tree/fromseq", "/home/vmadmin/pipeline"]
-output_folder = "fold_tree/fromseq"
+output_folder_name = "fold_tree/fromseq"
+output_folder = os.path.join(os.path.expanduser("~"), output_folder_name)  
 
 os.makedirs(output_folder, exist_ok=True)
 
@@ -25,3 +26,21 @@ with open(output_file, 'w') as f:
 
 
 print("Identifiers written to:", output_file)
+
+##COPY IDENTIFIERS.TXT TO RESULTS 
+import shutil
+
+ids_seq_dirname = "fold_tree/fromseq"
+ids_seq = os.path.join(os.path.expanduser("~"), ids_seq_dirname)
+source = os.path.join(ids_seq, "identifiers.txt")
+
+
+pipeline_ids_dirname = "protostructs/results/identifiers_seq.txt"
+pipeline_ids = os.path.join(os.path.expanduser("~"), pipeline_ids_dirname)
+destination = pipeline_ids
+
+try:
+    shutil.copy(source, destination)
+    print("identifiers copied to results folder")
+except OSError as e:
+    print("Error", e)
