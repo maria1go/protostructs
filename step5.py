c96c1e3e1d0a4b34dca7538d4090639c9da559ce
maria1go <165020879+maria1go@users.noreply.github.com>
2024-04-16 11:37:07 +0300

    Update step5.py
    Added output folder, os.path.expanduser is now used for paths

--- step5.py
+++ step5.py
@@ -1,8 +1,10 @@
 import subprocess
+import shutil
 import os 
 
 # Changing to the FoldTree directory
-dirname = "fold_tree"
+target_dirname = "fold_tree"
+dirname = os.path.join(os.path.expanduser("~"), target_dirname)
 os.chdir(dirname)
 
 
@@ -22,11 +24,16 @@ except subprocess.CalledProcessError as e:
     print("Error executing command:", e)
 
 
-###Enter .structs folder and copy to output folder?
-import shutil
+###Enter .structs folder and copy to output folder
 
-source = "fold_tree/fromseq/structs"
-destination = "pipeline/seq"
+structs_dirname = "fold_tree/fromseq/structs"
+structs = os.path.join(os.path.expanduser("~"), structs_dirname)
+source = structs
+
+pipeline_dirname = "protostructs/results"
+pipeline = os.path.join(os.path.expanduser("~"), pipeline_dirname)
+os.chdir(pipeline)
+destination = "seq"
 
 try:
     shutil.copytree(source, destination)
