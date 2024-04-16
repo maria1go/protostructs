0b96419f0e630ddb860f0a9f79f4232607fd45a9
maria1go <165020879+maria1go@users.noreply.github.com>
2024-04-16 23:58:28 +0300

    Update step9.py

--- step9.py
+++ step9.py
@@ -4,7 +4,7 @@ input_file = "results/str_alignment.fasta"
 output_file = "results/str_msa.fasta"
 
 def align_sequences(input_file, output_file):
-    mafft_command = f"/mafft --auto --reorder {input_file} > {output_file}"
+    mafft_command = f"mafft --auto --reorder {input_file} > {output_file}"
     subprocess.call(mafft_command, shell=True)
 
 def main():
