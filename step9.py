6b62aec71cb06de5e159febca8308fb652e70640
maria1go <165020879+maria1go@users.noreply.github.com>
2024-04-16 12:07:45 +0300

    Update step9.py
    Added output folder

--- step9.py
+++ step9.py
@@ -1,14 +1,14 @@
 import subprocess
 
-input_file = "str_alignment.fasta"
-output_file = "str_msa.fasta"
+input_file = "results/str_alignment.fasta"
+output_file = "results/str_msa.fasta"
 
 def align_sequences(input_file, output_file):
     mafft_command = f"/mafft --auto --reorder {input_file} > {output_file}"
     subprocess.call(mafft_command, shell=True)
 
 def main():
-    align_sequences("str_alignment.fasta", "str_msa.fasta")
+    align_sequences("results/str_alignment.fasta", "results/str_msa.fasta")
     print("MSA completed.", output_file)
 
 if __name__=="__main__":
